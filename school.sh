51ca918f75a088d30b23e42039d9eec08f80ecee
Ruotong Zhang <rzhang476@wisc.edu>
2023-10-19 13:47:22 -0500

    revise file to find average TotalAssessedValue

--- school.sh
+++ school.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d "," -f 7 | { sum=0; cnt=0; while read n; do sum=$(($sum + $n)); cnt=$(($cnt + 1)); done; avg=$(($sum / $cnt)); echo "sum of the column is $sum and average is $avg"; }
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d "," -f 7 | { sum=0; cnt=0; while read n; do sum=$(($sum + $n)); cnt=$(($cnt + 1)); done; f=$(echo "scale=6; $sum / $cnt" | bc); echo $f; }
